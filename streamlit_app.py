fd0bbbfa139ee98df810fc72d7e901f834951566
arierabr <rierabrellalfons@gmail.com>
2024-06-24 11:24:10 +0200

    changes 24 june 2024 from Fornells

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,9 +15,43 @@ st.title('🐡 Welcome to Blowfish!')
 
 st.header('Step 1: Choose your starting points')
 
-airports_df = pd.read_excel("data/airport_codes.xlsx")
+#all_iata_df = pd.read_excel("data/airport_codes.xlsx")
 
-selected_origin1 = st.selectbox("Choose an option:", airports_df.Nombre)
+aeropuertos_df = pd.read_csv("data/aeropuertos_v02.csv", encoding='utf-8')
+
+
+if 'counter' not in st.session_state:
+    st.session_state.counter = 0
+
+# Añadir tres campos de entrada
+col1, col2, col3, col4, col5 = st.columns(5)
+
+with col1:
+    data1 = st.selectbox("Country", aeropuertos_df.Country.unique())
+    data21 = st.selectbox("Country ", aeropuertos_df.Country.unique())
+with col2:
+    data2 = st.selectbox("City", aeropuertos_df[aeropuertos_df.Country == data1].City.unique())
+    data22 = st.selectbox("City ", aeropuertos_df[aeropuertos_df.Country == data21].City.unique())
+with col3:
+    data3 = st.number_input("# of passengers", step=1, min_value=0, format="%d")
+    data23 = st.number_input("# of passengers ", step=1, min_value=0, format="%d")
+with col4:
+    data4 = st.date_input("Departure date")
+    data24 = st.date_input("Departure date ")
+    if st.button("Add starting point"):
+        st.session_state.counter += 1
+with col5:
+    data5 = st.date_input("Arrival date")
+    data25 = st.date_input("Arrival date ")
+    if st.button("Drop starting point"):
+        st.session_state.counter -= 1
+
+
+
+
+st.write(f"Contador: {st.session_state.counter}")
+
+
 
 
 
